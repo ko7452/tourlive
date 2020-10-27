2bf79aab31757132ca810c67f55477f9a11cc547
KoYunHyuk <sksgur3217@gmail.com>
2020-10-27 22:49:31 +0900

    Updates

---
+++ precache-manifest.8c25dc8247573f42531a4b5f494d4984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b44ce7bc211490338138fdbdaad5a2",
+    "url": "/tourlive/index.html"
+  },
+  {
+    "revision": "4d5be9a8be87c7efde5d",
+    "url": "/tourlive/static/css/2.40563451.chunk.css"
+  },
+  {
+    "revision": "9c5134818365673e149b",
+    "url": "/tourlive/static/css/main.7a71d523.chunk.css"
+  },
+  {
+    "revision": "4d5be9a8be87c7efde5d",
+    "url": "/tourlive/static/js/2.a656e7d5.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/tourlive/static/js/2.a656e7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5134818365673e149b",
+    "url": "/tourlive/static/js/main.deca3a6d.chunk.js"
+  },
+  {
+    "revision": "4da5097b8ab0eea006aa",
+    "url": "/tourlive/static/js/runtime-main.c5d44a30.js"
+  }
+]);
